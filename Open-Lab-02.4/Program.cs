dd94c70a1329304494637e41dcd5b73e25bfc789
matejslovak <joggurtik1@gmail.com>
2019-10-28 08:04:56 +0100

    gg

--- Open-Lab-02.4/Program.cs
+++ Open-Lab-02.4/Program.cs
@@ -6,14 +6,18 @@ namespace Open_Lab_02._4
     {
         public static int GetLegsCount(int chickens, int cows, int pigs)
         {
-            return (chickens + cows + pigs);
+            return (chickens * 2 + cows * 4 + pigs * 4);
         }
         static void Main(string[] args)
         {
-            int chickenlegs = 2;
-            int cowslegs = 4;
-            int pigslegs = 4;
+            Console.WriteLine("Enter the count of cows");
+            int chickenlegs = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the count of chickens");
+            int cowslegs = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the count of pigs");
+            int pigslegs = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(GetLegsCount(chickenlegs, cowslegs, pigslegs));
+            
 
 
         }
